787c33b48ac96f5152c403818c3271f0d0d6498a
Fissehaye Girmay <fissehayegirmay@Fissehayes-MacBook-Pro-2.local>
2023-01-05 22:10:48 -0600

    Refactor tests

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,47 +1,67 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func TestFizzbuzz_Divisible_By_3(t *testing.T) {
-	actual := Fizzbuzz("12")
-	expected := "fizz"
-
-	if actual != expected {
-		t.Errorf("actual %s, expected %s", actual, expected)
+func TestFizzbuzz(t *testing.T) {
+	t.Helper()
+	cases := []struct{ Name, Input, Expected string} {
+		{ "Divisible by 3", "12", "fizz" },
+		{ "Divisible by 5", "10", "buzz" },
+		{ "Divisible by 3 and 5", "15", "fizzbuzz" },
+		{ "Not divisible by 5 and 3", "11", "not fizzy or buzzy" },
+		{ "Not a number", "abc", "abc1" },
+	}
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("actual %s, expected %s", tc.Input, tc.Expected), func(t *testing.T) {
+			actual := Fizzbuzz(tc.Input)
+			if actual != tc.Expected {t.Fatal("Failure")}
+		})
 	}
 }
 
-func TestFizzbuzz_Divisible_By_5(t *testing.T) {
-	actual := Fizzbuzz("10")
-	expected := "buzz"
+// func TestFizzbuzz_Divisible_By_3(t *testing.T) {
+// 	actual := Fizzbuzz("12")
+// 	expected := "fizz"
 
-	if actual != expected {
-		t.Errorf("actual %s, expected %s", actual, expected)
-	}
-}
-func TestFizzbuzz_Divisible_By_5_AND_3(t *testing.T) {
-	actual := Fizzbuzz("15")
-	expected := "fizzbuzz"
+// 	if actual != expected {
+// 		t.Errorf("actual %s, expected %s", actual, expected)
+// 	}
+// }
 
-	if actual != expected {
-		t.Errorf("actual %s, expected %s", actual, expected)
-	}
-}
+// func TestFizzbuzz_Divisible_By_5(t *testing.T) {
+// 	actual := Fizzbuzz("10")
+// 	expected := "buzz"
 
-func TestFizzbuzz_NOT_Divisible_By_5_AND_3(t *testing.T) {
-	actual := Fizzbuzz("14")
-	expected := "not fizzy or buzzy"
+// 	if actual != expected {
+// 		t.Errorf("actual %s, expected %s", actual, expected)
+// 	}
+// }
+// func TestFizzbuzz_Divisible_By_5_AND_3(t *testing.T) {
+// 	actual := Fizzbuzz("15")
+// 	expected := "fizzbuzz"
 
-	if actual != expected {
-		t.Errorf("actual %s, expected %s", actual, expected)
-	}
-}
+// 	if actual != expected {
+// 		t.Errorf("actual %s, expected %s", actual, expected)
+// 	}
+// }
 
-func TestFizzbuzz_NOT_Number(t *testing.T) {
-	actual := Fizzbuzz("abc")
-	expected := "abc"
+// func TestFizzbuzz_NOT_Divisible_By_5_AND_3(t *testing.T) {
+// 	actual := Fizzbuzz("14")
+// 	expected := "not fizzy or buzzy"
 
-	if actual != expected {
-		t.Errorf("actual %s, expected %s", actual, expected)
-	}
-}
+// 	if actual != expected {
+// 		t.Errorf("actual %s, expected %s", actual, expected)
+// 	}
+// }
+
+// func TestFizzbuzz_NOT_Number(t *testing.T) {
+// 	actual := Fizzbuzz("abc")
+// 	expected := "abc"
+
+// 	if actual != expected {
+// 		t.Errorf("actual %s, expected %s", actual, expected)
+// 	}
+// }
